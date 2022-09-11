12f029cc64801dba4726f7d49d13afaaed110dd9
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-09-11 01:16:37 +0100

    create layout header

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='h-16 px-20 py-2 shadow-xl'>
+      <div className=' h-full flex justify-start items-center'>
+        <span className='text-4xl'>CS in a Box</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
