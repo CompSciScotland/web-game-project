2073d1fc24a628d50f7095d031a8bc97eb6c5543
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-09-11 01:47:02 +0100

    add container

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import Header from './header'
+import Container from './container'
 
 const Layout = ({ children }) => {
-    return (
-        <div>
-            <Header />
-            <main className='h-screen'>{children}</main>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <main className='h-screen'>
+        <Container>
+          {children}
+        </Container>
+      </main>
+    </div>
+  )
 }
 
 export default Layout
